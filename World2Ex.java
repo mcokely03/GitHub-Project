01e38afa17e656524ccec91322290d3bcca9dec9
mcokely03 <meghancokely@dhcp-106-84.palace-wlan.simmons.edu>
2016-02-15 18:29:25 -0500

    Hello....

---
+++ World2Ex.java
@@ -0,0 +1,18 @@
+import java.util.*; //makes a scanner 
+
+
+class World2Ex{
+   public static void main(String argv[]){
+   Scanner input = new Scanner (System.in);
+   System.out.println("Enter a name");
+   String x = input.next ();
+   System.out.print("hello " + x);
+   
+   }
+}
+
+//system is physical system 
+//keyboard= system.in
+//screen= system.out
+// once you have scanner made you need to prompt the user for information. The input sucks 
+//the scanner in and it gives next string 
